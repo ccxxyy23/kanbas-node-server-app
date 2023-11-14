572643083f9f6cb71bd60a8091ebe0f0e9288044
ccxxyy23 <ccxxyy23@outlook.com>
2023-11-13 21:48:50 -0800

    first try

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 import express from "express";
 import Hello from "./hello.js";
 import Lab5 from "./lab5.js";
+import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./modules/routes.js";
 import cors from "cors";
+import "dotenv/config";
+import session from "express-session";
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+);
+app.use(express.json());
+CourseRoutes(app);
+ModuleRoutes(app);
 Lab5(app);
 Hello(app);
 app.listen(4000);
