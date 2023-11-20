4f532860f64882ba3a96c357ea83b0ad87a68c9e
ccxxyy23 <ccxxyy23@outlook.com>
2023-11-20 11:45:55 -0800

    add route for assignments and import it in app.js

--- app.js
+++ app.js
@@ -3,17 +3,21 @@ import Hello from "./hello.js";
 import Lab5 from "./lab5.js";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
+import AssignmentRoutes from "./assignments/routes.js";
 import cors from "cors";
 import "dotenv/config";
 
 const app = express();
-app.use(
-  cors({
-    credentials: true,
-    origin: process.env.FRONTEND_URL,
-  })
-);
+// app.use(
+//   cors({
+//     credentials: true,
+//     origin: process.env.FRONTEND_URL,
+//   })
+// );
+
+app.use(cors());
 app.use(express.json());
+AssignmentRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 Lab5(app);
