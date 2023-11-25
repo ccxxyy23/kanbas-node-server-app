f4457672f3577ca1c1ba95769533e5fe207b7569
ccxxyy23 <ccxxyy23@outlook.com>
2023-11-25 11:47:41 -0800

    import mangoose and connect to the db

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ import ModuleRoutes from "./modules/routes.js";
 import AssignmentRoutes from "./assignments/routes.js";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
 
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express();
 // app.use(
 //   cors({
