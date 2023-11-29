5301ef74f957c27001c2ea7c8ce01363b60806da
ccxxyy23 <ccxxyy23@outlook.com>
2023-11-28 22:23:15 -0800

    a6 Mongo fa23

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ import Lab5 from "./lab5.js";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import AssignmentRoutes from "./assignments/routes.js";
+import UserRoutes from "./users/routes.js";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import session from "express-session";
 
 mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express();
@@ -17,11 +19,33 @@ const app = express();
 //   })
 // );
 
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+);
+
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+
+app.use(session(sessionOptions));
 app.use(express.json());
 AssignmentRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
+UserRoutes(app);
 Lab5(app);
 Hello(app);
 app.listen(process.env.PORT || 4000);
